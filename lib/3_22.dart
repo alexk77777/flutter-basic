1c1668eb9bae6bb925fb9aa30f16d7c0995e81fc
Alex <misas777@gmail.com>
2021-03-22 10:49:20 +0900

    'second'

--- lib/3_22.dart
+++ lib/3_22.dart
@@ -1,32 +1,39 @@
 void main() {
-  int a = 10;
-  int b = 20;
+//   int a = 10;
+//   int b = 20;
+//
+//   int myAge = 15;
+//   int brotherAge = 30;
+//
+//   // int d = 5.5;
+//   double e = 10.5;
+//
+//   // [문자열]
+//   String name = '홍길동';
+//
+//   // [진위]
+//   bool isMarried = true;
+//   isMarried = false;
+//
+// //  [숫자목록]
+//   List<int> ageList = [10,20,30];
+//   var ageList1 = [10,20,30];
+//
+//   // [문자열 목록]
+//   List<String> nameList = ['홍길동, 한석봉'];
+//   // var nameList2
+//
+//   // null
+//   int g= 222;
+//
+//   String nem3 = '';
+//
+//
+  double tax = 1.1;
+  int fax = 5;
+  print('5만원짜리를 4만원으로 할인합니다.');
+  fax=4;
+  print('팩스의 새로운 가격은 (세금포함) ${fax*tax}만원!');
 
-  int myAge = 15;
-  int brotherAge = 30;
-
-  // int d = 5.5;
-  double e = 10.5;
-
-  // [문자열]
-  String name = '홍길동';
-
-  // [진위]
-  bool isMarried = true;
-  isMarried = false;
-
-//  [숫자목록]
-  List<int> ageList = [10,20,30];
-  var ageList1 = [10,20,30];
-
-  // [문자열 목록]
-  List<String> nameList = ['홍길동, 한석봉'];
-  var nameList2
-
-  // null
-  int g= 222
-
-  String nem3 = '';
-  String mane4
 
 }
