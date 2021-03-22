8eadd8864979398acd914385ce44a32a7a6ee185
Alex <misas777@gmail.com>
2021-03-22 10:28:33 +0900

    'first'

---
+++ test/sample.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {runApp(MyApp());}
+
+class MyApp extends StatelessWidget {
+  var size = MediaQuery.of(content).size;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
