f560062d71f399428e82ae4498d4106f29933181
Alex <misas777@gmail.com>
2021-04-05 20:26:25 +0900

    20210405_2026_update

--- lib/4.5.dart
+++ lib/4.5.dart
@@ -1,1 +1,47 @@
-g
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyAppPage(),
+    );
+  }
+}
+
+class MyAppPage extends StatefulWidget {
+  _MyAppPageState createState() => _MyAppPageState();
+}
+
+class _MyAppPageState extends State<MyAppPage> {
+  String _text = 'hello';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Hello World',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: Text(
+        _text,
+        style: TextStyle(fontSize: 20),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _text='World';
+          });
+        },
+        child: Icon(Icons.touch_app),
+      ),
+    );
+  }
+}
